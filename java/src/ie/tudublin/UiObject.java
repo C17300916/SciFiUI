0a3019b30cd611682f1bf2da333064259b3c5c4a
C17300916 <C17300916@mydit.ie>
2019-03-27 11:32:30 +0000

    added class for polymorphism

---
+++ java/src/ie/tudublin/UiObject.java
@@ -0,0 +1,13 @@
+package ie.tudublin;
+
+import processing.core.PVector;
+
+public abstract class UiObject
+{
+    
+    public abstract void update();
+
+    public abstract void render();
+
+    
+}
