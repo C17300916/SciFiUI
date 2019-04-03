f3a80a8b0309a3e79d2c54b585779d3baf146a6f
C17300916 <C17300916@mydit.ie>
2019-04-03 12:39:00 +0100

    successfully created array of colours using a 2D array

--- java/src/ie/tudublin/Planets.java
+++ java/src/ie/tudublin/Planets.java
@@ -1,11 +1,19 @@
 package ie.tudublin;
 
-import processing.core.PApplet;
-public class Planets extends UiObject
-{
-    int size = 50;
-    colorMode(HSB);
+import java.awt.*;
+import java.util.Random;
 
+public class Planets extends UiObject {
+    int size = 50;
+    int[][] planetcolours = {{255,0,0},{0,255,0},{0,0,255},{255,0,127},
+{102,0,204},{0,255,128},{51,255,255},{102,0,0}};
+    int[][] starcolours = {{255,255,51},{255,255,0},{255,128,0},{153,153}};
+
+
+    Random rand = new Random();
+    int Pnum = rand.nextInt(7);
+    int Snum = rand.nextInt(4);
+    
     public int fireRate;
 
 
@@ -22,7 +30,7 @@ public class Planets extends UiObject
         ui.pushMatrix();
         
         
-        
+        ui.stroke(planetcolours[Pnum][0],planetcolours[Pnum][1],planetcolours[Pnum][2]);
         ui.noFill();
         ui.line(400, 400, 10, 400);
         
