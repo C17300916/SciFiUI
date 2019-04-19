7c2dc3635ee9d2683666e1e1fa154ce0907b275f
C17300916 <C17300916@mydit.ie>
2019-04-19 12:43:00 +0100

    trying to plot distance graph

--- java/src/ie/tudublin/Distance.java
+++ java/src/ie/tudublin/Distance.java
@@ -1,25 +1,44 @@
 package ie.tudublin;
 
+import java.util.Random;
 
 public class Distance extends UiObject
 {
-    float h = 80;
+    float h = 160;
     float w = 400;
     float gap = 20;
+    Random rand = new Random();
     public Distance(UI ui, float x, float y)
     {
         super(ui, 0, x, y, 0, 0, 0);
     }
 
     float startPoint = x  - w/2 + 10;
-
+    float varY = y;
+    float prevY =y;
+    float nextPoint = startPoint + gap;
     @Override
 	public void update() {
         
         ui.stroke(255);
         ui.noFill();
         ui.rect(x, y, w, h);
-        ui.line(startPoint, y, startPoint + gap, y - 20);
+        for(int i=0; i< gap; i++){
+            ui.line(startPoint, prevY, nextPoint, varY);
+            startPoint = nextPoint;
+            prevY = varY;
+            nextPoint += gap;
+            if(varY == y){
+                varY += (h/2 + gap) + rand.nextFloat()*((-(h/2 - gap)) - (h/2 + gap));
+            }
+            if(varY > y){
+                varY -= (-(h/2 - gap)) + rand.nextFloat()*((varY) - (-(h/2 - gap)));
+            }
+            if(varY < y){
+                varY += (varY) + rand.nextFloat()*((h/2 + gap) - (varY));
+            }
+
+        }
         
         
 
