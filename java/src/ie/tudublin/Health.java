d208fc83e70e316d69e855613341a41b6fa9bbb2
C17300916 <C17300916@mydit.ie>
2019-04-23 17:15:03 +0100

    health image decrements over time

--- java/src/ie/tudublin/Health.java
+++ java/src/ie/tudublin/Health.java
@@ -10,22 +10,41 @@ public class Health extends UiObject
 
     float a = 50;
     float b = 200;
+    //changeable variables for rect 1
+    float aChange1 = 0; 
+    float bChange1 =0;
+    //changeable variables for rect 2
+    float aChange2 = 0; 
+    float bChange2 =0;
+    //setting all health items to 100%
     float brain = 100;
     float body =100;
     float lungs =100;
     float fNw =100;//food and water
 
+
     public void render()
     {
 
-        //adding health cross shape
+        //everything inside large circle for design
+        ui.fill(0,255,0);
+        ui.circle(x, y, b +a );
+
+        //adding health cross shape (inside colour)
         ui.beginShape();
-        ui.stroke(255);
-        ui.fill(255);
+        ui.stroke(0,0,255);
+        ui.fill(0,0,255);
         ui.rectMode(ui.CENTER);
         ui.rect(x, y, a, b);
         ui.rect(x,y,b,a);
-
+        ui.endShape();
+        //health shape (outside colour)
+        ui.beginShape();
+        ui.stroke(255);
+        ui.fill(255);
+        ui.rectMode(ui.CORNER);
+        ui.rect(x -a/2, y - b/2, a, bChange1);
+        ui.rect(x - b/2 ,y - a/2 ,b, aChange2);
         ui.endShape();
 
         //adding text indicators
@@ -52,18 +71,54 @@ public class Health extends UiObject
         if(BodTime >= 2.0){
             body --;
             BodTime =0;
+            if(bChange1 <=b/2 +a/2){
+                bChange1 ++;
+            }
+            else{
+                bChange1 ++;
+                if(aChange2 <= a){
+                    aChange2 ++;
+                }
+            }
         }
         if(BrainTime  >= 5.0){
             brain --;
             BrainTime =0;
+            if(bChange1 <=b/2 +a/2){
+                bChange1 ++;
+            }
+            else{
+                bChange1 ++;
+                if(aChange2 <= a){
+                    aChange2 ++;
+                }
+            }
         }
         if(fnWTime >= 20.0){
             fNw --;
             fnWTime =0;
+            if(bChange1 <=b/2 +a/2){
+                bChange1 ++;
+            }
+            else{
+                bChange1 ++;
+                if(aChange2 <= a){
+                    aChange2 ++;
+                }
+            }
         }
         if(lungsTime >= 17.0){
             lungs --;
             lungsTime =0;
+            if(bChange1 <=b/2 +a/2){
+                bChange1 ++;
+            }
+            else{
+                bChange1 ++;
+                if(aChange2 <= a){
+                    aChange2 ++;
+                }
+            }
         }
 		
     }
