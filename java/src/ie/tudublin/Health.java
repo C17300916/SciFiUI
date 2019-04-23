967031979b58a9e2d1bb7f991a8c048fe32fe206
C17300916 <C17300916@mydit.ie>
2019-04-23 16:47:59 +0100

    four health objects deteriorate over time

--- java/src/ie/tudublin/Health.java
+++ java/src/ie/tudublin/Health.java
@@ -10,6 +10,10 @@ public class Health extends UiObject
 
     float a = 50;
     float b = 200;
+    float brain = 100;
+    float body =100;
+    float lungs =100;
+    float fNw =100;//food and water
 
     public void render()
     {
@@ -25,11 +29,46 @@ public class Health extends UiObject
         ui.endShape();
 
         //adding text indicators
+        ui.text("BRAIN", x, y);
+        ui.text(brain + "%", x, y);
+        ui.text("BODY", x, y);
+        ui.text(body + "%", x, y);
+        ui.text("LUNGS", x, y);
+        ui.text(lungs + "%", x, y);
+        ui.text("FOOD & WATER", x, y);
+        ui.text(fNw + "%", x, y);
+
 
         
     }
 
 	public void update() {
+        //health deteriorating over time
+        BodTime += ui.timeDelta;
+        BrainTime += ui.timeDelta;
+        fnWTime += ui.timeDelta;
+        lungsTime += ui.timeDelta;
+
+        if(BodTime >= 2.0){
+            body --;
+            BodTime =0;
+        }
+        if(BrainTime  >= 5.0){
+            brain --;
+            BrainTime =0;
+        }
+        if(fnWTime >= 20.0){
+            fNw --;
+            fnWTime =0;
+        }
+        if(lungsTime >= 17.0){
+            lungs --;
+            lungsTime =0;
+        }
 		
     }
+    float BodTime;
+    float fnWTime;
+    float lungsTime;
+    float BrainTime;
 }
